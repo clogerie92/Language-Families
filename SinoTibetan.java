8e6b1d8d973c93f394eab82ef8064ec0b9d8e00e
clogerie <66565287+clogerie@users.noreply.github.com>
2022-10-27 11:56:45 -0400

    added if statement to change word order pending if language name has 'chinese' in it

--- SinoTibetan.java
+++ SinoTibetan.java
@@ -1,5 +1,8 @@
 public class SinoTibetan extends Language {
     SinoTibetan(String langName, int speakers) {
         super(langName, speakers, "Asia", "subject-object-verb");
+        if(langName.contains("Chinese")) {
+            this.wordOrder = "subject-verb-object";
+        }
     }
 }
